fe9f338c264227e0cb91b765bb5ef8eb43b53f1f
Zackptg5 <zackptg5@comcast.net>
2018-08-30 21:12:48 -0400

    unity v1.6.1 update

--- common/install.sh
+++ common/install.sh
@@ -1,3 +1,6 @@
+ui_print "   Decompressing files..."
+tar -xf $INSTALLER/custom/$API.tar.xz -C $INSTALLER/custom 2>/dev/null
+
 rm -rf /data/app/de.robv.android.xposed.installer /magisk/xposed* /sbin/.core/img/xposed*
 if [ -f "/data/app/de.robv.android.xposed.installer/de.robv.android.xposed.installer.apk" ]; then
   [ "$(cmp /data/app/de.robv.android.xposed.installer/de.robv.android.xposed.installer.apk $INSTALLER/system/app/XposedFramework/XposedFramework.apk)" ] && rm -rf /data/data/de.robv.android.xposed.installer
@@ -9,7 +12,7 @@ if $MAGISK && ! $SYSOVERRIDE; then
   mv -f $INSTALLER/system/bin/app_process32_magisk $INSTALLER/system/bin/app_process32
   $IS64BIT && mv -f $INSTALLER/system/bin/app_process64_magisk $INSTALLER/system/bin/app_process64
   rm -f $INSTALLER/system/xposed.prop
-  cp_ch_nb $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
+  cp_ch -n $INSTALLER/custom/$API/$ARCH/xposed.prop $UNITY/xposed.prop
 else
   rm -f $INSTALLER/system/bin/app_process32_magisk
   $IS64BIT && rm -f $INSTALLER/system/bin/app_process64_magisk
